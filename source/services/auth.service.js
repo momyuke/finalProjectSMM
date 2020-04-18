c923cc23d7e3bc39f2ca2984ad666dc22d43c718
Teguh Triprasetya <triprasetyat@gmail.com>
2020-04-18 16:36:53 +0700

    [AUTH]

--- source/services/auth.service.js
+++ source/services/auth.service.js
@@ -5,17 +5,21 @@ const logEvent = require('../event/myEmitter');
 const rp = require('request-promise');
 const axios = require('axios').default;
 
+function getRedisToken() {
+    let token = await rp.post('http://ec2-18-136-210-143.ap-southeast-1.compute.amazonaws.com:3333/token');
+    token = JSON.parse(token);
+    return { user: checkData, token: token.result }
+}
+
 class AuthLogin {
-    async authenticateGoogleSignIn(user){
+    async authenticateGoogleSignIn(user) {
         let result;
         try {
-            const checkData = await User.findOne({where : {email : user.email}});
-            if(!checkData){
-                result = {message : 'Email is not valid', status : 401};
-            }else{
-                let token = await rp.post('http://ec2-18-136-210-143.ap-southeast-1.compute.amazonaws.com:3333/token');
-                token = JSON.parse(token);
-                result = {user : checkData, token : token.result}
+            const checkData = await User.findOne({ where: { email: user.email } });
+            if (!checkData) {
+                result = { message: 'Email is not valid', status: 401 };
+            } else {
+                result = getRedisToken()
             }
         } catch (e) {
             logEvent.emit('APP_INFO', {
@@ -36,20 +40,13 @@ class AuthLogin {
         try {
             userLogin = await User.findOne({ where: { email: user.email } });
             if (!userLogin) {
-                result = {message : "Email is not valid", status : 401};
+                result = { message: "Email is not valid", status: 401 };
             } else {
                 const matchPassword = bcrypt.compareSync(user.password, userLogin.password);
                 if (matchPassword) {
-                    const expiresIn = 10000;
-                    const accessToken = jwt.sign({ id: userLogin.id }, process.env.SECRET_KEY, {
-                        expiresIn: expiresIn
-                    });
-                    result = {
-                        userLogin,
-                        token: accessToken
-                    }
+                    result = getRedisToken();
                 } else {
-                    result = {message : "Password is wrong", status: 403};
+                    result = { message: "Password is wrong", status: 403 };
                 }
             }
         } catch (err) {
